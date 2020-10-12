19d9f06a86b49f46bcbd78384b0e9d1f63cad8e5
Arles <goldenprieto@gmail.com>
2020-10-12 17:44:36 -0500

    Listo para Produccion

--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -1,21 +1,27 @@
 var socket = io();
+
 var params = new URLSearchParams(window.location.search);
 
-if(!params.has('nombre') || !params.has('sala')){
-    window.location= 'index.html';
-    throw new Error('EL nombre y sala son necesario');
+if (!params.has('nombre') || !params.has('sala')) {
+    window.location = 'index.html';
+    throw new Error('El nombre y sala son necesarios');
 }
 
 var usuario = {
-    nombre : params.get('nombre'),
-    sala:params.get('sala')
-}
+    nombre: params.get('nombre'),
+    sala: params.get('sala')
+};
+
+
 
 socket.on('connect', function() {
     console.log('Conectado al servidor');
-    socket.emit('entrarChat',usuario,function(resp){
-        console.log('Usuarios Conectados ',resp);
+
+    socket.emit('entrarChat', usuario, function(resp) {
+        //console.log('Usuarios conectados', resp);
+        renderizarUsuarios(resp,false);
     });
+
 });
 
 // escuchar
@@ -26,9 +32,9 @@ socket.on('disconnect', function() {
 });
 
 
-// // Enviar información
-// socket.emit('enviarMensaje', {
-//     usuario: 'Fernando',
+// Enviar información
+// socket.emit('crearMensaje', {
+//     nombre: 'Fernando',
 //     mensaje: 'Hola Mundo'
 // }, function(resp) {
 //     console.log('respuesta server: ', resp);
@@ -36,20 +42,20 @@ socket.on('disconnect', function() {
 
 // Escuchar información
 socket.on('crearMensaje', function(mensaje) {
-
-    console.log('Servidor:', mensaje);
-
+    // console.log('Servidor:', mensaje);
+    renderizarMensajes(mensaje);
+    scrollBottom();
 });
 
-// escuchar cambios de usuarios 
+// Escuchar cambios de usuarios
 // cuando un usuario entra o sale del chat
-socket.on('listaPersona',function(personas){
-    console.log(personas);
+socket.on('listaPersona', function(personas) {
+        renderizarUsuarios(personas);
 });
 
-//mensajes privados
-socket.on('mensajePrivado',function(mensaje){
-    console.log('Mensaje Privado', mensaje);
+// Mensajes privados
+socket.on('mensajePrivado', function(mensaje) {
+
+    console.log('Mensaje Privado:', mensaje);
+
 });
-
-
--- server/sockets/socket.js
+++ server/sockets/socket.js
@@ -22,16 +22,21 @@ io.on('connection', (client) => {
         usuarios.agregarPersona(client.id,data.nombre,data.sala);
 
         client.broadcast.to(data.sala).emit('listaPersona',usuarios.getPersonasPorSala(data.sala));
+        client.broadcast.to(data.sala).emit('crearMensaje',crearMensaje('Admin',`${data.nombre} Se acaba de Conectar`));    
 
         callback(usuarios.getPersonasPorSala(data.sala));
         //console.log(usuario);
 
     });
     // el servidor esta esperando que algun cliente envie un mensaje
-    client.on('crearMensaje',(data)=>{
+    client.on('crearMensaje',(data,callback)=>{
         let persona = usuarios.getPersona(client.id);
+
         let mensaje = crearMensaje(persona.nombre, data.mensaje);
         client.broadcast.to(persona.sala).emit('crearMensaje',mensaje);
+        
+
+        callback(mensaje);
     })
 
 
